03cd1bf284337a3c37a71ec3482a03b0b58aa86b
TomMakesThings <61354833+TomMakesThings@users.noreply.github.com>
2021-05-28 16:31:24 +0100

    Search bar suggestions

--- Javascript/graph_search.js
+++ Javascript/graph_search.js
@@ -6,12 +6,8 @@ function submitted(event) {
   const url = "https://github.com/TomMakesThings/Clustering-and-TDA-of-scRNA-seq-Data/tree/gh-pages"
   const win = window.open(url, '_blank');
   win.focus();
+  hideDropdown()
 }
-
-graphform.addEventListener('submit', submitted);
-
-graphform.addEventListener("mouseover", showDropdown);
-graphform.addEventListener("mouseout", hideDropdown);
 
 function showDropdown(event){
   document.getElementById("dropdown-content").style.display = "block";
@@ -21,18 +17,38 @@ function hideDropdown(event) {
   document.getElementById("dropdown-content").style.display = "none";
 }
 
-function filterFunction() {
-  // var input, filter, ul, li, a, i;
-  // input = document.getElementById("query");
-  // filter = input.value.toUpperCase();
-  // div = document.getElementById("graphform");
-  // a = div.getElementsByTagName("a");
-  // for (i = 0; i < a.length; i++) {
-  //   txtValue = a[i].textContent || a[i].innerText;
-  //   if (txtValue.toUpperCase().indexOf(filter) > -1) {
-  //     a[i].style.display = "";
-  //   } else {
-  //     a[i].style.display = "none";
-  //   }
-  // }
+function searchFilter() {
+  input = query.value.toUpperCase();
+  form = document.getElementById("graphform");
+  let options = ["cluster-link", "tda-link"];
+  let categories = ["cluster-category", "tda-category"];
+  for (i = 0; i < options.length; i++) {
+    option = form.getElementsByClassName(options[i]);
+    category = document.getElementById(categories[i]);
+    category_empty = true
+    for (j = 0; j < option.length; j++) {
+      text_value = option[j].textContent || option[j].innerText;
+      text_tags = option[j].dataset.tags;
+      /* Check if the input text matches the option's name or metadata tags */
+      if (text_value.toUpperCase().indexOf(input) > -1 || text_tags.toUpperCase().indexOf(input) > -1) {
+        option[j].style.display = "";
+        category_empty = false;
+      }
+      /* Otherwise hide the option */
+      else {
+        option[j].style.display = "none";
+      }
+    }
+    /* Hide category headers if no options left*/
+    if (category_empty) {
+      category.style.display = "none";
+    }
+    else {
+      category.style.display = "";
+    }
+  }
 }
+
+graphform.addEventListener('submit', submitted);
+graphform.addEventListener("mouseover", showDropdown);
+graphform.addEventListener("mouseout", hideDropdown);
