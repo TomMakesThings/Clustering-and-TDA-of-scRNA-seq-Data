f54dbb4117a3847bde7825267fe28f54ae328fe8
TomMakesThings <61354833+TomMakesThings@users.noreply.github.com>
2021-05-28 13:08:36 +0100

    Added graph drop down

--- Javascript/graph_search.js
+++ Javascript/graph_search.js
@@ -9,3 +9,30 @@ function submitted(event) {
 }
 
 graphform.addEventListener('submit', submitted);
+
+graphform.addEventListener("mouseover", showDropdown);
+graphform.addEventListener("mouseout", hideDropdown);
+
+function showDropdown(event){
+  document.getElementById("dropdown-content").style.display = "block";
+}
+
+function hideDropdown(event) {
+  document.getElementById("dropdown-content").style.display = "none";
+}
+
+function filterFunction() {
+  // var input, filter, ul, li, a, i;
+  // input = document.getElementById("query");
+  // filter = input.value.toUpperCase();
+  // div = document.getElementById("graphform");
+  // a = div.getElementsByTagName("a");
+  // for (i = 0; i < a.length; i++) {
+  //   txtValue = a[i].textContent || a[i].innerText;
+  //   if (txtValue.toUpperCase().indexOf(filter) > -1) {
+  //     a[i].style.display = "";
+  //   } else {
+  //     a[i].style.display = "none";
+  //   }
+  // }
+}
