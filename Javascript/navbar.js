215c9d1d62c591db0ed30c28f9ff6fb4cc9b7962
TomMakesThings <61354833+TomMakesThings@users.noreply.github.com>
2021-05-29 23:05:18 +0100

    Added GIF

---
+++ Javascript/navbar.js
@@ -0,0 +1,7 @@
+function profileHover(element) {
+  element.setAttribute('src', 'Assets/Images/Profile-Wink.gif');
+}
+
+function profileUnhover(element) {
+  element.setAttribute('src', 'Assets/Images/Profile.jpg');
+}
